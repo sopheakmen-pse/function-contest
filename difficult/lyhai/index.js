addab2ec20c178b9951a8d70a673ba85c9b9507f
lynakimhay <nakimhay.ly@institute.pse.ngo>
2024-03-22 14:52:51 +0700

    function

---
+++ difficult/lyhai/index.js
@@ -0,0 +1,18 @@
+let students = 100 ;
+
+function students () {
+    if (students <= 50){
+        console.log ='E';
+    } else if(students <= 70){
+        console.log ='D';
+    } if (students <= 80){
+        console.log ='C';
+    } else if (students <= 90){
+        console.log ='B';
+    } if (students <= 100){
+        console.log ='A';
+    }
+
+    return students ;
+
+};
