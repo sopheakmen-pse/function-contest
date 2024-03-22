0d72b771f08030e6df2edf8cce1a328c82da0385
pisethmararika <mararyka.piseth@institute.pse.ngo>
2024-03-22 14:39:11 +0700

    rika

---
+++ easy/index.js
@@ -0,0 +1,10 @@
+function Product(ProductA, ProductB, ProductC) {
+    let priceA = 3;
+    let priceB = 5;
+    let priceC = 6;
+
+    let totalprice = (ProductA * priceA) + (ProductB * priceB) + (ProductC * priceC);
+
+    return totalprice;
+}
+
